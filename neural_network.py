53fee13b7ebc066c8e434c260f237b6410df4573
Medvedev Ivan <31549278+vanokako@users.noreply.github.com>
2021-03-16 13:04:54 +0300

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -36,7 +36,7 @@ def build_model(input_shape, output_length):
     tf.keras.layers.Dense(units=128),
     tf.keras.layers.LeakyReLU(),
     tf.keras.layers.Dropout(0.05),
-    tf.keras.layers.Dense(units=output_length, activation="linear"),
+    tf.keras.layers.Dense(units=output_length),
 ],name="Larger_network",)
     # model = Sequential()
     # model.add(Dense(16, input_shape = (input_shape,),activation='relu', use_bias=False))
@@ -57,29 +57,38 @@ def build_model(input_shape, output_length):
     return model
 
 def make_prediction(data, gaps, max, min):
-    batch_size = 10
+    batch_size = 4
     x, y, predict_data = prepare_for_traning(data, gaps)
     x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.2)
 
     print(x_test.shape, y_test.shape)
     print(x_train.shape, y_train.shape)
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss')
+    checkpoint_filepath = './checkpoint'
+    best_model = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=True,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+
+
     input_shape = x_train.shape[1]
     output_length = data.shape[1]-len(predict_data)
     model = build_model(input_shape, output_length)
     history = model.fit(x_train, y_train,
                         epochs=1000,
                         validation_data=(x_test, y_test),
-                        verbose = 0,
+                        verbose = 1,
                         batch_size=batch_size,
-                        callbacks=[early_stopping]
+                        callbacks=[best_model]
                         )
-
+    model.load_weights(checkpoint_filepath)
+    print(history.history['val_loss'])
     a = model.predict(x_test)
     print("++++++++++++++TEST++++++++++")
-    print(y_test[0], y_test[1])
+    print(y_test[:5]*(max-min)+min)
     print("++++++++++++++PRREDICT++++++++++")
-    print(a[0],a[1])
+    print(a[:5]*(max-min)+min)
 
     # print(y_test)
     # print(a)
